ce58a8e631a48f4ec6bad6227e80d7e1c6b70e60
Andrew Hyder <hyd415@gmail.com>
2014-05-19 13:02:53 -0700

    Upload files to S3

--- open_trails/routes.py
+++ open_trails/routes.py
@@ -1,5 +1,5 @@
 from open_trails import app
-from open_trails.upload import upload_s3, download_s3
+from open_trails.upload import upload_to_s3, download_s3
 from open_trails.transformers import unzipfile
 from flask import request, render_template
 import json
@@ -10,15 +10,15 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    data_url = upload_s3(request.form['org-name'], request.files['file'])
+    s3_file_url = upload_to_s3(request.form['org-name'], request.files['file'])
 
 # @app.route('/map', methods=['POST'])
 # def map():
 #     data = transformers.transform_shapefile(shapefile)
 #     return render_template('map.html', data = data)
 
-@app.route('/map/<org_name>')
-def map_existing_org(org_name):
-    import pdb; pdb.set_trace()
-    filepath = download_s3(org_name)
-    return unzipfile(filepath)
+# @app.route('/map/<org_name>')
+# def map_existing_org(org_name):
+#     import pdb; pdb.set_trace()
+#     filepath = download_s3(org_name)
+#     return unzipfile(filepath)
