1f8e8811a17d4546390d3a822992e5e3b06caa4b
Andrew Hyder <hyd415@gmail.com>
2014-05-23 12:32:18 -0700

    Commented out file upload tests, and started testing new steward info

--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
 # -- coding: utf-8 --
-from shutil import rmtree
+from shutil import rmtree, copy
 from unittest import TestCase, main
 from os.path import join, dirname
+import os
 from urlparse import urljoin
 from tempfile import mkdtemp
-
 from bs4 import BeautifulSoup
 
 from open_trails import app, transformers
+from open_trails.functions import unzip
 
 class FakeUpload:
     ''' Pretend to be a file upload in flask.
@@ -24,7 +25,13 @@ class TestTransformers (TestCase):
 
     def setUp(self):
         self.tmp = mkdtemp(prefix='plats-')
-        app.config['UPLOAD_FOLDER'] = self.tmp
+        names = ('test-files/lake-man.zip',
+                 'test-files/lake-man-GGNRA.zip',
+                 'test-files/lake-man-San-Antonio.zip',
+                 'test-files/lake-man-Santa-Clara.zip',
+                 'test-files/lake-man-Portland.zip')
+        for name in names:
+            copy(name, self.tmp)
 
     def tearDown(self):
         rmtree(self.tmp)
@@ -32,20 +39,15 @@ class TestTransformers (TestCase):
     def testConvert(self):
         ''' Test basic SHP to GeoJSON conversion.
         '''
-        names = ('test-files/lake-man.zip',
-                 'test-files/lake-man-GGNRA.zip',
-                 'test-files/lake-man-San-Antonio.zip',
-                 'test-files/lake-man-Santa-Clara.zip',
-                 'test-files/lake-man-Portland.zip')
+        for name in os.listdir(self.tmp):
+            path = unzip(join(self.tmp,name))
+            self.doFileConversion(path)
 
-        for name in names:
-            self.doFileConversion(name)
-
-    def doFileConversion(self, name):
+    def doFileConversion(self, path):
         ''' Test conversion results for named file.
         '''
-        file = FakeUpload(join(dirname(__file__), name))
-        geojson = transformers.transform_shapefile(file)
+        file = join(dirname(__file__), path)
+        geojson = transformers.transform_shapefile(path)
 
         #
         # Is it GeoJSON?
@@ -66,56 +68,79 @@ class TestTransformers (TestCase):
         self.assertTrue(37.80071 < min(lats) and max(lats) < 37.80436)
         self.assertTrue(-122.25925 < min(lons) and max(lons) < -122.25671)
 
+
+def whooo():
+    print "WHOOOOO"
+
 class TestApp (TestCase):
 
     def setUp(self):
+        app.config.update(DATASTORE='testing')
         self.app = app.test_client()
         self.tmp = mkdtemp(prefix='plats-')
-        app.config['UPLOAD_FOLDER'] = self.tmp
-
-    def tearDown(self):
-        rmtree(self.tmp)
-
-    def testUpload(self):
-        ''' Check basic file upload flow.
-        '''
         names = ('test-files/lake-man.zip',
                  'test-files/lake-man-GGNRA.zip',
                  'test-files/lake-man-San-Antonio.zip',
                  'test-files/lake-man-Santa-Clara.zip',
                  'test-files/lake-man-Portland.zip')
-
         for name in names:
-            self.doUpload(name)
+            copy(name, self.tmp)
 
-    def doUpload(self, name):
-        ''' Check basic file upload flow for named file.
+    def tearDown(self):
+        rmtree(self.tmp)
+
+    #@patch('open_trails.routes.new_steward')
+    def test_new_stewards(self):
+        '''Test creating a new stewards.csv
         '''
-        response = self.app.get('/')
+        data = {
+            "name" : "Test Steward",
+            "email" : "testemail@email.com",
+            "url" : "http://testurl.com",
+            "phone" : "123456789"
+            }
+        #import pdb; pdb.set_trace()
+        response = self.app.post('/new-steward', data=data, follow_redirects=True)
+        #mock_new_steward.make_folders.assert_called()
         self.assertEqual(response.status_code, 200)
-
-        #
-        # Check for a file upload field in the home page form.
-        #
-        soup = BeautifulSoup(response.data)
-        form = soup.find('input', attrs=dict(type='file')).find_parent('form')
-        self.assertTrue('multipart/form-data' in form['enctype'])
-        self.assertTrue(form.find_all('input', attrs=dict(type='file')))
-
-        #
-        # Attempt to upload a test shapefile.
-        #
-        action = urljoin('/', form['action'])
-        input = form.find('input', attrs=dict(type='file'))['name']
-        file = open(join(dirname(__file__), name))
-        response = self.app.post(action, data={input: file})
-
-        self.assertEqual(response.status_code, 200)
-
-    def testOpenThreeZippedShapefiles(self):
-        ''' Tests openning of a zipfile containing three zipped shapefiles
-        '''
-        self.doUpload("test-files/sa-test-files.zip")
+    #
+    #
+    #
+    # def testUpload(self):
+    #     ''' Check basic file upload flow.
+    #     '''
+    #
+    #     for name in os.listdir(self.tmp):
+    #         self.doUpload(name)
+    #
+    # def doUpload(self, name):
+    #     ''' Check basic file upload flow for named file.
+    #     '''
+    #     response = self.app.get('/')
+    #     self.assertEqual(response.status_code, 200)
+    #
+    #     #
+    #     # Check for a file upload field in the home page form.
+    #     #
+    #     soup = BeautifulSoup(response.data)
+    #     form = soup.find('input', attrs=dict(type='file')).find_parent('form')
+    #     self.assertTrue('multipart/form-data' in form['enctype'])
+    #     self.assertTrue(form.find_all('input', attrs=dict(type='file')))
+    #
+    #     #
+    #     # Attempt to upload a test shapefile.
+    #     #
+    #     action = urljoin('/', form['action'])
+    #     input = form.find('input', attrs=dict(type='file'))['name']
+    #     file = open(join(dirname(__file__), name))
+    #     response = self.app.post(action, data={input: file})
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def testOpenThreeZippedShapefiles(self):
+    #     ''' Tests openning of a zipfile containing three zipped shapefiles
+    #     '''
+    #     self.doUpload("test-files/sa-test-files.zip")
 
 if __name__ == '__main__':
     main()
